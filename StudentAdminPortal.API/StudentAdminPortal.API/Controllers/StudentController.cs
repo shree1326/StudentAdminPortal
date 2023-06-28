e294c5797457421cfe89f44d1a2b380e739ab724
shree1326 <shreyansi1326@gmail.com>
2023-06-28 13:09:37 +0530

    End of Section 2 - API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+
+        public StudentController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudentsAsync()
+        {
+            var students = await studentRepository.GetStudentsAsync();
+
+            #region CodeWithoutAutoMapper
+            //var domainModelStudent = new List<Student>();
+            //foreach (var student in students)
+            //{
+            //    domainModelStudent.Add(new Student()
+            //    {
+            //        Id = student.Id,
+            //        FirstName = student.FirstName,
+            //        LastName = student.LastName,
+            //        DateOfBirth = student.DateOfBirth,
+            //        Email = student.Email,
+            //        Mobile = student.Mobile,
+            //        ProfileImageUrl = student.ProfileImageUrl,
+            //        GenderId = student.GenderId,
+            //        Address = new Address()
+            //        {
+            //            Id = student.Address.Id,
+            //            PhysicalAddress = student.Address.PhysicalAddress,
+            //            PostalAddress = student.Address.PostalAddress,
+            //        },
+            //        Gender = new Gender()
+            //        {
+            //            GenderId = student.Gender.GenderId,
+            //            Description = student.Gender.Description
+            //        }
+            //    });
+
+            //}
+            #endregion
+
+            return Ok(mapper.Map<List<Student>>(students));
+        }
+    }
+}
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DomainModels/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DomainModels
+{
+    public class Gender
+    {
+        public Guid GenderId { get; set; }
+        public string Description { get; set; }
+    }
+}
